08c1cf974fab5ef9cca668d49ab3645269b7ab38
Yurena González <yurena.gonher@educacyl.jcyl.es>
2025-04-30 11:42:45 +0200

    Creada vista para reseteo.

---
+++ src/jcolonia/daw2024/e3a/VistaReseteo.java
@@ -0,0 +1,18 @@
+package jcolonia.daw2024.e3a;
+
+/**
+ * Vista: Utilidades reseteo del contenido de un inventario.
+ * 
+ * @see ControladorInventarioAula
+ * 
+ * @version 1 (20250430000)
+ * @author Yurena González
+ */
+public class VistaReseteo extends VistaGeneral {
+
+	public VistaReseteo(String nombre) {
+		super(nombre);
+		
+	}
+
+}
